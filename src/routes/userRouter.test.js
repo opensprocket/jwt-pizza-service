f5adb33f66f3ca20d714cb53261b55eda84e0c21
opensprocket <opensprocket@users.noreply.github.com>
2026-02-22 20:46:20 -0700

    remove unused code

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -1,10 +1,6 @@
 const request = require('supertest');
 const app = require('../service.js');
-// const { DB } = require('../database/database.js');
-// const jwt = require('jsonwebtoken');
-// const config = require('../config.js');
 
-// const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a'};
 
 test('list users unauthorized', async () => {
   const listUsersRes = await request(app).get('/api/user');
