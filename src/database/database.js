0c3b2d7f3a1c71fc9a6e140a1858cac5a4a3da6b
opensprocket <opensprocket@users.noreply.github.com>
2026-03-25 20:24:05 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const logger = require('../logger.js');
 
 class DB {
   constructor() {
@@ -362,6 +363,7 @@ class DB {
   }
 
   async query(connection, sql, params) {
+    logger.dbLog(sql, params);
     const [results] = await connection.execute(sql, params);
     return results;
   }
